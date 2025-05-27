8211b23b6fcb1213c414319772e24210acd9b819
Richard Souza <souzza.richard25@gmail.com>
2025-05-27 20:43:49 -0300

    feat: Models User

--- src/backend/models/user.py
+++ src/backend/models/user.py
@@ -1,21 +1,24 @@
+from typing import Optional
+
 from pydantic import BaseModel
 from sqlmodel import Field, SQLModel
 
 
 class User(SQLModel, table=True):
-    id:       int  = Field(nullable=False, primary_key=True)
-    name:     str  = Field(nullable=False                  )
-    username: str  = Field(nullable=False, index=True      )
-    password: str  = Field(nullable=False                  )
-    phone:    str  = Field(nullable=True                   )
-    excluded: bool = Field(nullable=False, default=False   )
+    __tablename__ = "tb_user"
+
+    id:          Optional[int] = Field(default=None, primary_key=True)
+    name:        str  = Field(nullable=False               )
+    username:    str  = Field(nullable=False, index=True   )
+    password:    str  = Field(nullable=False               )
+    phone:       str  = Field(nullable=True                )
+    fl_excluded: bool = Field(nullable=False, default=False)
 
 
-class UserDto(BaseModel):
+
+class UserCreate(SQLModel):
     name:     str
     username: str
     password: str
-    phone:    str | None = None
+    phone:    Optional[str] = None
 
-    class Config:
-        orm_mode = True
